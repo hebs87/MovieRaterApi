d27d88818fd9557d32cea335aedd3624b174eb59
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-03-19 22:57:22 +0000

    Created test POST method in MovieViewSet

--- api/views.py
+++ api/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import Movie, Rating
 from .serializers import MovieSerializer, RatingSerializer
 
@@ -11,6 +13,18 @@ class MovieViewSet(viewsets.ModelViewSet):
     """
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+
+    # The decorator stipulates that we are allowing only the POST method on the function,
+    # and the detail=True means that we can only call it on a specific movie (ID), so we
+    # will need to provide the ID of the movie we want to call it on. False would mean it
+    # applies to the whole list of movies
+    @action(detail=True, methods=['POST'])
+    def rate_movie(self, request, pk=None):
+        """
+        A function that allows the user to rate a movie
+        """
+        response = {'message': 'It\'s working'}
+        return Response(response, status=status.HTTP_200_OK)
 
 
 class RatingViewSet(viewsets.ModelViewSet):
