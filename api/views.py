d0d7a0bdb6f28767f7d6f8d6e161183db447dfd7
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-03-19 23:29:50 +0000

    Updated function in MoveViewSet with rating create and update functionality

--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -21,16 +22,45 @@ class MovieViewSet(viewsets.ModelViewSet):
     @action(detail=True, methods=['POST'])
     def rate_movie(self, request, pk=None):
         """
-        A function that allows the user to rate a movie
+        A function that allows the user to rate a movie - if a record already exists,
+        the existing rating will be updated. If not, a new record will be created
         """
         if 'stars' in request.data:
             # We display this message if there are stars in request to rate the movie
             # and we print the movie title if the request is successful
             movie = Movie.objects.get(id=pk)
-            print('Movie title:', movie.title)
+            stars = request.data['stars']
+            # user = request.user
+            # As we currently don't have authentication, we can just specify a dummy user ID
+            user = User.objects.get(id=1)
 
-            response = {'message': 'It\'s working'}
-            return Response(response, status=status.HTTP_200_OK)
+            # If we have something in the database for the user and movie combo already, we
+            # will update the existing record. If not, then we will create a new record
+            try:
+                # We get the relevant rating record based on the user ID and movie ID
+                rating = Rating.objects.get(user=user.id, movie=movie.id)
+                # Update the stars rating with the new rating
+                rating.stars = stars
+                rating.save()
+                # We serialize the rating data, specify that it is only one record (many=False)
+                serializer = RatingSerializer(rating, many=False)
+                # We display an object with the success message and the serialized data
+                response = {
+                    'message': 'Rating updated',
+                    'result': serializer.data
+                }
+                return Response(response, status=status.HTTP_200_OK)
+            except:
+                # We create a new record and pass in the user, movie and stars values
+                rating = Rating.objects.create(user=user, movie=movie, stars=stars)
+                # We serialize the rating data, specify that it is only one record (many=False)
+                serializer = RatingSerializer(rating, many=False)
+                # We display an object with the success message and the serialized data
+                response = {
+                    'message': 'Rating created',
+                    'result': serializer.data
+                }
+                return Response(response, status=status.HTTP_200_OK)
         else:
             # We display this message if the user hasn't provided a star rating
             response = {'message': 'You need to provide stars'}
