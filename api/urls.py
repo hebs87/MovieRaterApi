c4291d8cc73b35f78e47e510c72868305d5b835f
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-03-19 15:10:45 +0000

    Registered the viewsets in url router

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,14 @@
 from django.urls import include, path
 from rest_framework import routers
+from .views import MovieViewSet, RatingViewSet
 
 # Use router to register our ViewSets as urls (REST framework)
 router = routers.DefaultRouter()
+# We register our viewsets
+# The first argument is the string that will be passed into the URL path
+# The second argument is the viewset itself
+router.register('movies', MovieViewSet)
+router.register('ratings', RatingViewSet)
 
 urlpatterns = [
     # Create a path to include our router urls
