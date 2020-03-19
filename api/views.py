98cf4ee148bfbd1a15bf5dd4589b73010bf2bfd8
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-03-19 15:10:09 +0000

    Created viewsets for Movie and Rating models and serializers to enable our API view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Movie, Rating
+from .serializers import MovieSerializer, RatingSerializer
+
 
 # Create your views here.
+class MovieViewSet(viewsets.ModelViewSet):
+    """
+    A viewset to render the Movie model and serializer
+    in an API format
+    """
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+
+class RatingViewSet(viewsets.ModelViewSet):
+    """
+    A viewset to render the Rating model and serializer
+    in an API format
+    """
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
